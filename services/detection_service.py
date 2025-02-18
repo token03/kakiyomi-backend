391c2e4133e921afc7b06e9b031fc924a526b00f
Jessie Zhong <zhongjessie2003@gmail.com>
2025-02-17 23:57:20 -0800

    fix detection model path

--- services/detection_service.py
+++ services/detection_service.py
@@ -7,18 +7,19 @@ import largestinteriorrectangle as lir
 import numpy as np
 import cv2
 
+
 class DetectionService:
     def __init__(self):
         print("Initializing Detection Service")
-        self.BUBBLE_MODEL_PATH = '../models/detection/comic-speech-bubble-detector.pt'
-        self.TEXT_SEG_MODEL_PATH = '../models/detection/comic-text-segmenter.pt'
-        self.TEXT_DETECT_MODEL_PATH = '../models/detection/manga-text-detector.pt'
+        self.BUBBLE_MODEL_PATH = "./models/detection/comic-speech-bubble-detector.pt"
+        self.TEXT_SEG_MODEL_PATH = "./models/detection/comic-text-segmenter.pt"
+        self.TEXT_DETECT_MODEL_PATH = "./models/detection/manga-text-detector.pt"
 
         self.bubble_detection = YOLO(self.BUBBLE_MODEL_PATH)
         self.text_segmentation = YOLO(self.TEXT_SEG_MODEL_PATH)
         self.text_detection = YOLO(self.TEXT_DETECT_MODEL_PATH)
 
-        self.device = 'cpu'  # TODO: add support for GPU
+        self.device = "cpu"  # TODO: add support for GPU
         print("Detection Service initialized")
 
     def clean_up(self):
@@ -33,27 +34,47 @@ class DetectionService:
         size = (h, w) if h >= w * 5 else 1024
         det_size = (h, w) if h >= w * 5 else 640
 
-        bubble_detec_result = self.bubble_detection(img, device=self.device, imgsz=size, conf=0.1, verbose=False)[0]
-        txt_seg_result = self.text_segmentation(img, device=self.device, imgsz=size, conf=0.1, verbose=False)[0]
-        txt_detect_result = self.text_detection(img, device=self.device, imgsz=det_size, conf=0.2, verbose=False)[0]
+        bubble_detec_result = self.bubble_detection(
+            img, device=self.device, imgsz=size, conf=0.1, verbose=False
+        )[0]
+        txt_seg_result = self.text_segmentation(
+            img, device=self.device, imgsz=size, conf=0.1, verbose=False
+        )[0]
+        txt_detect_result = self.text_detection(
+            img, device=self.device, imgsz=det_size, conf=0.2, verbose=False
+        )[0]
 
-        combined = self.combine_results(bubble_detec_result, txt_seg_result, txt_detect_result, img)
+        combined = self.combine_results(
+            bubble_detec_result, txt_seg_result, txt_detect_result, img
+        )
 
-        blk_list = [TextBlock(txt_bbox, bble_bbox, txt_class, inp_bboxes)
-                for txt_bbox, bble_bbox, inp_bboxes, txt_class in combined]
+        blk_list = [
+            TextBlock(txt_bbox, bble_bbox, txt_class, inp_bboxes)
+            for txt_bbox, bble_bbox, inp_bboxes, txt_class in combined
+        ]
 
-        # sort the blocks (rtl if japanese)  
-        rtl = True if source_lang == 'Japanese' else False
+        # sort the blocks (rtl if japanese)
+        rtl = True if source_lang == "Japanese" else False
         sorted_result = sort_blk_list(blk_list, rtl)
 
         return sorted_result
 
-    def combine_results(self, bubble_detec_results, text_seg_results, text_detect_results, image):
-        bubble_bounding_boxes = np.array(bubble_detec_results.boxes.xyxy.cpu(), dtype="int")
-        seg_text_bounding_boxes = np.array(text_seg_results.boxes.xyxy.cpu(), dtype="int")
-        detect_text_bounding_boxes = np.array(text_detect_results.boxes.xyxy.cpu(), dtype="int")
+    def combine_results(
+        self, bubble_detec_results, text_seg_results, text_detect_results, image
+    ):
+        bubble_bounding_boxes = np.array(
+            bubble_detec_results.boxes.xyxy.cpu(), dtype="int"
+        )
+        seg_text_bounding_boxes = np.array(
+            text_seg_results.boxes.xyxy.cpu(), dtype="int"
+        )
+        detect_text_bounding_boxes = np.array(
+            text_detect_results.boxes.xyxy.cpu(), dtype="int"
+        )
 
-        text_bounding_boxes = self.merge_bounding_boxes(seg_text_bounding_boxes, detect_text_bounding_boxes)
+        text_bounding_boxes = self.merge_bounding_boxes(
+            seg_text_bounding_boxes, detect_text_bounding_boxes
+        )
         text_bounding_boxes = self.filter_bounding_boxes(text_bounding_boxes)
 
         text_blocks_bboxes = []
@@ -68,15 +89,31 @@ class DetectionService:
             for txt_idx, txt_box in enumerate(text_bounding_boxes):
                 for bble_box in bubble_bounding_boxes:
                     if does_rectangle_fit(bble_box, txt_box):
-                        raw_results.append((txt_box, bble_box, text_blocks_bboxes[txt_idx], 'text_bubble'))
+                        raw_results.append(
+                            (
+                                txt_box,
+                                bble_box,
+                                text_blocks_bboxes[txt_idx],
+                                "text_bubble",
+                            )
+                        )
                         text_matched[txt_idx] = True
                         break
                     elif self.do_rectangles_overlap(bble_box, txt_box):
-                        raw_results.append((txt_box, bble_box, text_blocks_bboxes[txt_idx], 'text_free'))
+                        raw_results.append(
+                            (
+                                txt_box,
+                                bble_box,
+                                text_blocks_bboxes[txt_idx],
+                                "text_free",
+                            )
+                        )
                         text_matched[txt_idx] = True
                         break
                 if not text_matched[txt_idx]:
-                    raw_results.append((txt_box, None, text_blocks_bboxes[txt_idx], 'text_free'))
+                    raw_results.append(
+                        (txt_box, None, text_blocks_bboxes[txt_idx], "text_free")
+                    )
 
         return raw_results
 
@@ -112,7 +149,7 @@ class DetectionService:
             min(box1[0], box2[0]),
             min(box1[1], box2[1]),
             max(box1[2], box2[2]),
-            max(box1[3], box2[3])
+            max(box1[3], box2[3]),
         ]
 
     def merge_bounding_boxes(self, seg_boxes, detect_boxes):
@@ -128,7 +165,9 @@ class DetectionService:
         for detect_box in detect_boxes:
             add_box = True
             for merged_box in merged_boxes:
-                if self.do_rectangles_overlap(detect_box, merged_box, 0.1) or does_rectangle_fit(merged_box, detect_box):
+                if self.do_rectangles_overlap(
+                    detect_box, merged_box, 0.1
+                ) or does_rectangle_fit(merged_box, detect_box):
                     add_box = False
                     break
             if add_box:
@@ -139,7 +178,9 @@ class DetectionService:
             for j, other_box in enumerate(merged_boxes):
                 if i == j:
                     continue
-                if self.do_rectangles_overlap(running_box, other_box, 0.1) and not does_rectangle_fit(running_box, other_box):
+                if self.do_rectangles_overlap(
+                    running_box, other_box, 0.1
+                ) and not does_rectangle_fit(running_box, other_box):
                     running_box = self.merge_boxes(running_box, other_box)
             final_boxes.append(running_box)
         unique_boxes = []
@@ -147,7 +188,9 @@ class DetectionService:
         for box in final_boxes:
             duplicate = False
             for seen_box in seen_boxes:
-                if (np.array_equal(box, seen_box) or self.do_rectangles_overlap(box, seen_box, 0.6)):
+                if np.array_equal(box, seen_box) or self.do_rectangles_overlap(
+                    box, seen_box, 0.6
+                ):
                     duplicate = True
                     break
             if not duplicate:
@@ -165,8 +208,12 @@ class DetectionService:
         binary_black_text = cv2.adaptiveThreshold(
             gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
         )
-        num_labels_white, labels_white, stats_white, centroids_white = cv2.connectedComponentsWithStats(binary_white_text, connectivity=8)
-        num_labels_black, labels_black, stats_black, centroids_black = cv2.connectedComponentsWithStats(binary_black_text, connectivity=8)
+        num_labels_white, labels_white, stats_white, centroids_white = (
+            cv2.connectedComponentsWithStats(binary_white_text, connectivity=8)
+        )
+        num_labels_black, labels_black, stats_black, centroids_black = (
+            cv2.connectedComponentsWithStats(binary_black_text, connectivity=8)
+        )
         min_area = 10
         content_bboxes = []
         height, width = image.shape[:2]
@@ -197,12 +244,19 @@ class DetectionService:
     def filter_bounding_boxes(self, bboxes, width_tolerance=5, height_tolerance=5):
         def is_close(value1, value2, tolerance):
             return abs(value1 - value2) <= tolerance
+
         return [
-            bbox for bbox in bboxes
-            if not (is_close(bbox[0], bbox[2], width_tolerance) or is_close(bbox[1], bbox[3], height_tolerance))
+            bbox
+            for bbox in bboxes
+            if not (
+                is_close(bbox[0], bbox[2], width_tolerance)
+                or is_close(bbox[1], bbox[3], height_tolerance)
+            )
         ]
 
-    def adjust_contrast_brightness(self, img: np.ndarray, contrast: float = 1.0, brightness: int = 0):
+    def adjust_contrast_brightness(
+        self, img: np.ndarray, contrast: float = 1.0, brightness: int = 0
+    ):
         brightness += int(round(255 * (1 - contrast) / 2))
         return cv2.addWeighted(img, contrast, img, 0, brightness)
 
@@ -216,7 +270,9 @@ class DetectionService:
         blurred = cv2.GaussianBlur(image, (5, 5), 0)
         edges = cv2.Canny(blurred, 50, 150)
         contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        stage_1 = cv2.drawContours(np.zeros_like(image), contours, -1, (255, 255, 255), thickness=2)
+        stage_1 = cv2.drawContours(
+            np.zeros_like(image), contours, -1, (255, 255, 255), thickness=2
+        )
         stage_1 = cv2.bitwise_not(stage_1)
         stage_1 = cv2.cvtColor(stage_1, cv2.COLOR_BGR2GRAY)
         _, binary_image = cv2.threshold(stage_1, 200, 255, cv2.THRESH_BINARY)
@@ -232,7 +288,9 @@ class DetectionService:
     def bubble_contour(self, frame_mask: np.ndarray):
         gray = self.ensure_gray(frame_mask)
         ret, thresh = cv2.threshold(gray, 200, 255, 0)
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, hierarchy = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+        )
         largest_contour = max(contours, key=cv2.contourArea)
         return largest_contour
 
@@ -243,3 +301,4 @@ class DetectionService:
         x1, y1 = lir.pt1(rect)
         x2, y2 = lir.pt2(rect)
         return x1, y1, x2, y2
+
